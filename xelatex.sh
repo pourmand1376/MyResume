2d68a8c5bcbc6ea22b4cc16efcc41c454d8fbe0d
Amir Pourmand <pourmand1376@gmail.com>
2024-01-16 09:48:49 +0000

    update bash script

--- xelatex.sh
+++ xelatex.sh
@@ -1,5 +1,5 @@
-sudo apt update
-sudo apt-get install texlive-xetex
-sudo apt install texlive-fonts-extra
+sudo apt update -y
+sudo apt-get install texlive-xetex -y
+sudo apt install texlive-fonts-extra -y 
 (cd industry && xelatex resume.tex)
 (cd academic && xelatex CirrculumVitae.tex)
